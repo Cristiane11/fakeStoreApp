ce299862258a2e22be679b333aeb46646bc2a765
Cristiane11 <bjwmcd@yahoo.com>
2025-07-31 15:07:21 -0400

    update

--- fakestore-app/src/App.jsx
+++ fakestore-app/src/App.jsx
@@ -1,9 +1,10 @@
 //import { useState } from 'react'
 import Home from './components/Home'
-import AddProduct from './components/AddProduct'
-import ProductDetails from './components/ProductDetails'
-import ProductList from './components/ProductList'
-
+import NavigationBar from './components/NavigationBar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddProduct from './components/AddProduct';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
 import './App.css'
 
 function App() {
@@ -11,11 +12,18 @@ function App() {
   
   return (
     <>
-    <Home/>
-    <AddProduct />
-    <ProductDetails/>
-    <ProductList/>
-     
+    <NavigationBar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        <Route path="/products" element={<ProductList />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+        <Route path="add-product" element={<AddProduct/>} />
+        
+        </Route>
+      </Routes>
+    </BrowserRouter>
+   
     </>
   )
 }
