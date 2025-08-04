8a6a4e5800f88e8250343a734a01bb0120d004e8
Cristiane11 <bjwmcd@yahoo.com>
2025-08-04 10:16:24 -0400

    update

--- fakestore-app/src/components/ProductDetails.jsx
+++ fakestore-app/src/components/ProductDetails.jsx
@@ -4,10 +4,8 @@ import axios from "axios";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 
-
-
-function ProductDetails() {
- const{id}= useParams();
+const ProductDetails = ()=> {
+ const {id} = useParams();
  const[product, setProduct] = useState(null);
  const[loading, setLoading]= useState(true);
  const[error,setError]= useState(null);
@@ -22,10 +20,24 @@ useEffect(()=>{
     setError("Failed to Fetch Products");
     setLoading(false);
   })
-},[id])
+},[id]);
+if (loading) return <p>Loading Products...</p>;
+if (error) return <p>{error}</p>;
+
    return (
     <>
-      <h1 className="mt-5">Products Details</h1>
+      <Container>
+        <Card>
+          <Card.Img variant="top" src={product.image} alt={product.title}/>
+          <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>{product.description} | <span> {product.category}</span></Card.Text>
+              <Card.Text>${product.price}</Card.Text>
+              <Button variant="primary">Buy Now</Button>
+          </Card.Body>
+        </Card>
+      </Container>
+
     </>
   )
 }
